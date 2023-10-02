1d5d6fb3a881f2955bd28d360a75250e0faa5ea2
leona-art <leonagraph@gmail.com>
2023-10-02 22:30:45 +0900

    [feat]surrealdbセットアップ

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use once_cell::sync::Lazy;
+use surrealdb::{
+    engine::local::{Db, RocksDb},
+    Surreal,
+};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+static DB: Lazy<Surreal<Db>> = Lazy::new(Surreal::init);
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
+        .setup(|_| {
+            tauri::async_runtime::block_on(async {
+                DB.connect::<RocksDb>("./data.db").await.unwrap();
+                DB.use_ns("personal").use_db("my-note").await.unwrap();
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
