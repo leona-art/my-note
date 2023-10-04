0ed602e6ffb8094bbf18767747c58a7d60181988
leona-art <leonagraph@gmail.com>
2023-10-04 21:38:46 +0900

    [test]create,show

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,121 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::result;
+
 use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use surrealdb::{
     engine::local::{Db, RocksDb},
+    opt::IntoResource,
+    sql::Thing,
     Surreal,
 };
 
+/// 全体で共通のデータベースインスタンス
 static DB: Lazy<Surreal<Db>> = Lazy::new(Surreal::init);
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![create_topic])
+        // DBの使用準備
         .setup(|_| {
             tauri::async_runtime::block_on(async {
-                DB.connect::<RocksDb>("./data.db").await.unwrap();
+                let mut dir = std::env::current_dir().unwrap();
+                dir.push("database.db");
+                DB.connect::<RocksDb>(dir).await.unwrap();
                 DB.use_ns("personal").use_db("my-note").await.unwrap();
             });
             Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct Topic {
+    title: String,
+    content: String,
+}
+impl Topic {
+    fn new(title: &str, content: Option<&str>) -> Topic {
+        Topic {
+            title: title.to_string(),
+            content: match content {
+                Some(s) => s.to_string(),
+                None => String::new(),
+            },
+        }
+    }
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct TopicWithId {
+    id: Thing,
+    #[serde(flatten)]
+    topic: Topic,
+}
+
+#[tauri::command]
+async fn create_topic(title: &str, content: Option<&str>) -> Result<String, String> {
+    let result: Vec<TopicWithId> = DB
+        .create("topic")
+        .content(Topic::new(title, content))
+        .await
+        .unwrap();
+    Ok(result[0].id.to_raw())
+}
+
+#[tauri::command]
+async fn show_topic(id: Option<&str>) -> Result<(), String> {
+    let resource = match id {
+        Some(id) => id,
+        None => "topic",
+    };
+
+    let mut resource = resource.split(":");
+    match (resource.next(), resource.next()) {
+        (Some(tb), Some(id)) => {
+            let result: Option<TopicWithId> = DB.select((tb, id)).await.unwrap();
+        }
+        (Some(tb), None) => {
+            let result: Vec<TopicWithId> = DB.select(tb).await.unwrap();
+        }
+        _ => panic!("idの形式が違います。"),
+    }
+
+    Ok(())
+}
+
+mod test {
+    use std::vec;
+
+    use super::*;
+
+    #[test]
+    fn connect_test() {
+        tauri::async_runtime::block_on(async {
+            let mut dir = std::env::current_dir().unwrap();
+            dir.push("test.db");
+            DB.connect::<RocksDb>(dir.clone()).await.unwrap();
+            DB.use_ns("test").use_db("test").await.unwrap();
+            std::fs::remove_dir_all(dir).unwrap();
+        });
+    }
+
+    #[test]
+    fn create_topic_test() {
+        tauri::async_runtime::block_on(async {
+            let mut dir = std::env::current_dir().unwrap();
+            dir.push("test.db");
+            DB.connect::<RocksDb>(dir.clone()).await.unwrap();
+            DB.use_ns("test").use_db("test").await.unwrap();
+
+            create_topic("title", Some("content")).await.unwrap();
+
+            std::fs::remove_dir_all(dir).unwrap();
+        });
+    }
+
+    #[test]
+    fn show_topics() {}
 }
