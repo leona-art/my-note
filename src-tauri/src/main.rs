be0f46f60e6538264134b3b9b070d74afad1d62c
leona-art <leonagraph@gmail.com>
2023-10-04 21:44:35 +0900

    [feat]topic取得

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::result;
+use std::{result, vec};
 
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
@@ -66,7 +66,7 @@ async fn create_topic(title: &str, content: Option<&str>) -> Result<String, Stri
 }
 
 #[tauri::command]
-async fn show_topic(id: Option<&str>) -> Result<(), String> {
+async fn show_topic(id: Option<&str>) -> Result<Vec<TopicWithId>, String> {
     let resource = match id {
         Some(id) => id,
         None => "topic",
@@ -76,14 +76,17 @@ async fn show_topic(id: Option<&str>) -> Result<(), String> {
     match (resource.next(), resource.next()) {
         (Some(tb), Some(id)) => {
             let result: Option<TopicWithId> = DB.select((tb, id)).await.unwrap();
+            Ok(match result {
+                Some(topic) => vec![topic],
+                None => Vec::new(),
+            })
         }
         (Some(tb), None) => {
-            let result: Vec<TopicWithId> = DB.select(tb).await.unwrap();
+            let result = DB.select::<Vec<TopicWithId>>(tb).await.unwrap();
+            Ok(result)
         }
-        _ => panic!("idの形式が違います。"),
+        _ => Err("idの形式が違います。".to_string()),
     }
-
-    Ok(())
 }
 
 mod test {
